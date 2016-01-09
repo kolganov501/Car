833e1430553f75f4714852e67b00fa327249bd40
kolganov501 <kolganov501@yandex.ru>
2016-01-09 08:20:17 +0600

    Доработал ваши пожелания

--- car2/car/Program.cs
+++ car2/car/Program.cs
@@ -6,43 +6,58 @@ using System.Threading.Tasks;
 
 namespace car
 {
-    // Так, давай по порядку
-    // Имя класса с большой буквы.
-    public class car
+                                                            // Так, давай по порядку
+                                                            // Имя класса с большой буквы.
+    public class Сar
     {
-        // _something - это правило именования приватных полей
-        // например private int _currentSpeed;
-        // не забывай, что досутп к приватным членам класса (_currentSpeed) должен быть
-        //реализовать через свойства, CurrentSpeed  (есть в Троелсене)
+                                                            // _something - это правило именования приватных полей
+                                                            // например private int _currentSpeed;
+                                                            // не забывай, что досутп к приватным членам класса (_currentSpeed) должен быть
+                                                            //реализовать через свойства, CurrentSpeed  (есть в Троелсене)
 
-        // еще замечание - я не уверен, что внешнее окружение может постоянно имзенять maxSpeed
-        // максимальная скорость - устанавливается заводом изготовителем твоей машины и задается один раз при создании
-        public int _cursp, _maxsp = 26;
-
-        // тут тоже самое public srting Name {get; set;}
-        // private string _name;
-        public string _name = "a";
-
-        public car(string name, int maxsp)
+                                                            // еще замечание - я не уверен, что внешнее окружение может постоянно имзенять maxSpeed
+                                                            // максимальная скорость - устанавливается заводом изготовителем твоей машины и задается один раз при создании
+        private int _maxSpeed = 26, _curSpeed;
+        private string _name;
+        public int CurSpeed //Нам это свойство в данной задаче не нужно ведь мы не используем приватные переменные вне класса
         {
-            // в целом верно, но this можно не использовать
-            this._name = name;
-            this._maxsp = maxsp;
+            get { return _curSpeed; }
+            set { _curSpeed = value; }
         }
 
-        // Так, тут тоже все нормально, добавим условий
-        // если машина достигла своего максимума, то выдается сообщение, что мы перегрелись
-        // и скорость начинает падать до нуля 
+                                                            // тут тоже самое public srting Name {get; set;}
+                                                            // private string _name;
+        public string Name { get; set; }//то свойство по сути не связанно с private string _name;
+
+        public Сar(string name, int maxsp)
+        {
+                                                            // в целом верно, но this можно не использовать
+            _name = name;
+            _maxSpeed = maxsp;
+        }
+
+                                                            // Так, тут тоже все нормально, добавим условий
+                                                            // если машина достигла своего максимума, то выдается сообщение, что мы перегрелись
+                                                            // и скорость начинает падать до нуля 
         public void accel()
         {
-            for (_cursp = 0; _cursp <= _maxsp; _cursp += 5)
-                Console.WriteLine("Current speed: " + _cursp);
+            for (_curSpeed = 0; _curSpeed <= _maxSpeed; _curSpeed += 5)
+            {
+                if (_curSpeed == _maxSpeed)
+                {
+                    Console.WriteLine("Перегрев!");
+                    _curSpeed = 0;
+                    Console.WriteLine("Current speed: " + _curSpeed);
+                    break;
+                }
+                Console.WriteLine("Current speed: " + _curSpeed);
+            }
         }
 
-        // круто, что переопределил ToString - молодец! :)
+                                                            // круто, что переопределил ToString - молодец! :)
         public override string ToString()
         {
-            return "Name: " + _name + "; Max speed: " + _maxsp;
+            return "Name: " + _name + "; Max speed: " + _maxSpeed;
         }
         
     }
@@ -52,9 +67,9 @@ namespace car
         {
             string name = Console.ReadLine();
             int maxsp = Convert.ToInt32(Console.ReadLine());
-            car c1 = new car(name, maxsp);
-            c1.accel();
+            Сar c1 = new Сar(name, maxsp);
             Console.WriteLine(c1.ToString());
+            c1.accel();            
             Console.ReadLine();
         }
     }
